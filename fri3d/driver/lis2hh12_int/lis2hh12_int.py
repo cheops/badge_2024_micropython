154db18d141bd5f0f912b0f148e6fdffbd71c4d8
Joram Agten <joram.agten@vito.be>
2024-04-05 02:55:29 +0200

    url with extra functionality

--- fri3d/driver/lis2hh12_int/lis2hh12_int.py
+++ fri3d/driver/lis2hh12_int/lis2hh12_int.py
@@ -1,5 +1,8 @@
 import lis2hh12.py
 
+# extra functionality look at
+# https://github.com/tinypico/tinypico-micropython/tree/master/lis3dh%20library
+
 class LIS2HH12_int(lis2hh12.LIS2HH12):
     def enable_act_int(self):
         self._register_char(_CTRL6, 0x0A)
