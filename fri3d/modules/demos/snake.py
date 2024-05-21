a24ee774f27c61cb1fac17a9a8570aa1c1697107
Joram Agten <joram.agten@vito.be>
2024-05-21 15:02:29 +0200

    use buttons.py

--- fri3d/modules/demos/snake.py
+++ fri3d/modules/demos/snake.py
@@ -27,71 +27,7 @@ BUTTON_Y_PIN = const(41)
 BUTTON_MENU_PIN = const(45)
 BUTTON_START_PIN = const(0)
 
-class DebouncedButton:
-    """
-    Debounced pin handler
-
-    - example usage with callback
-    ```
-        def button_pressed(btn:str):
-            print(f"Button {btn} pressed.")
-        db_y = DebouncedButton(Pin(BUTTON_Y_PIN, Pin.IN, Pin.PULL_UP), button_pressed, "Y")
-    ```
-    when button Y is pressed, the function button_pressed will get the argument "Y"
-
-    - example usage to query the debounced value
-    'db_y.value()' will return 1 when button Y is pressed down
-    
-    """
-
-    def __init__(self, pin, cb=None, arg=None, debounce=200):
-        self._pin = pin
-        self.cb = cb
-        self.arg = arg
-        self.debounce = debounce
-        self._start = time.ticks_ms()
-        self._state = 0               # holds the current button state (1 = pressed)
-        self._deb_passed = True       # temp var to check of debounce time has expired
-        self._value = 1               # temp var to buffer pin.value()
-
-        pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self.debounce_handler)
-    
-    def value(self):
-        self._deb_passed = time.ticks_diff(time.ticks_ms(), self._start) > self.debounce
-        if self._state == 1 and self._deb_passed:
-            self._value == self._pin.value()
-            if self._value == 1:
-                self._state = 0
-        return self._state
-
-    def set_callback_arg(cb=None, arg=None):
-        self.cb = cb
-        self.arg = arg
-
-    def debounce_handler(self, pin):
-        """debounce the pin"""
-        self._value = pin.value()
-        self._deb_passed = time.ticks_diff(time.ticks_ms(), self._start) > self.debounce
-        
-        if self._value == 0:
-            # press
-            if self._state == 0:
-                self._state = 1
-                self._start = time.ticks_ms()
-                if not self.cb is None:
-                    micropython.schedule(self.cb, self.arg)  # Schedule the function func to be executed “very soon”, outside irq
-            else:
-                # double trigger, ignore
-                if self._deb_passed:
-                    # we might have missed the up during debounce
-                    self._start = time.ticks_ms()
-                    if not self.cb is None:
-                        micropython.schedule(self.cb, self.arg)  # Schedule the function func to be executed “very soon”, outside irq
-        else:
-            # release
-            if self._deb_passed:
-                self._state = 0
-
+from buttons import DebouncedButton
 
 db_a = DebouncedButton(Pin(BUTTON_A_PIN, Pin.IN, Pin.PULL_UP))
 db_b = DebouncedButton(Pin(BUTTON_B_PIN, Pin.IN, Pin.PULL_UP))
