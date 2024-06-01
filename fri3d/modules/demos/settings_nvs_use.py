cb0a0698cca4f74058572a21fec3b5fc2105ba53
Joram Agten <joram.agten@vito.be>
2024-06-02 01:28:35 +0200

    demo nvs settings

---
+++ fri3d/modules/demos/settings_nvs_use.py
@@ -0,0 +1,24 @@
+from fri3d.settings_nvs import write_blob, read_blob, dict_to_toml_blob, toml_blob_to_dict
+
+settings = {
+    "system": {
+        "wifi": {
+            "aps": [
+                {"ssid": "your-ssid", "key": "your-key"}
+                ]
+            }
+        }
+    }
+
+print(settings)
+
+write_blob("system", "settings", dict_to_toml_blob(settings))
+
+settings_2 = toml_blob_to_dict(read_blob("system", "settings"))
+
+print(settings_2)
+
+if settings != settings_2:
+    print("test failed")
+else:
+    print("test ok")
