b94af85b91cc044c9fcc8a28f53f1ecbc284720d
Joram Agten <joram.agten@vito.be>
2024-06-24 14:58:21 +0200

    proper import

--- fri3d/driver/lis2hh12_int/lis2hh12_int.py
+++ fri3d/driver/lis2hh12_int/lis2hh12_int.py
@@ -1,8 +1,8 @@
-import lis2hh12.py
+import lis2hh12
 
 # extra functionality look at
 # https://github.com/tinypico/tinypico-micropython/tree/master/lis3dh%20library
 
 class LIS2HH12_int(lis2hh12.LIS2HH12):
     def enable_act_int(self):
-        self._register_char(_CTRL6, 0x0A)
+        self._register_char(lis2hh12._CTRL6, 0x0A)
