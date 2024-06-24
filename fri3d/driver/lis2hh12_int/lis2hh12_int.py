1d2bab07aa8375020b9c81d38a3adcc2a1c37416
Joram Agten <joram.agten@vito.be>
2024-06-24 15:03:39 +0200

    documentation

--- fri3d/driver/lis2hh12_int/lis2hh12_int.py
+++ fri3d/driver/lis2hh12_int/lis2hh12_int.py
@@ -1,6 +1,9 @@
 import lis2hh12
 
-# extra functionality look at
+# this is an extenstion to lis2hh12 driver from https://github.com/tuupola/micropython-lis2hh12
+# to enable the interrupt pin, so that badge-2022 display is on
+
+# if you want to implement extra functionality (tapped, shaken) look at
 # https://github.com/tinypico/tinypico-micropython/tree/master/lis3dh%20library
 
 class LIS2HH12_int(lis2hh12.LIS2HH12):
